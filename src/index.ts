a0202f0c28378af99b2ade3b4aa7cf6bfd01a2a5
MiguelCastanedaDev <mickeycasta4@gmail.com>
2026-09-18 14:55:34 -0600

    Implement user authentication with registration and login endpoints; add bcrypt for password hashing and comparison

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,77 @@
 import { Hono } from 'hono'
+import { hashPassword } from './utils/hashPassword';
+import { comparePassword } from './utils/comparePassword';
+import type { Bindings } from './types/types';
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post('/auth/register', async (c) => {
+  const { email, password } = await c.req.json();
 
+  if (!email || !password) {
+    return c.json({ message: 'Email and password are required' }, 400);
+  }
+
+  // Aquí deberías generar el hash de password
+  const passwordHash = await hashPassword(password);
+
+  const result = await c.env.DB
+    .prepare(`
+      INSERT INTO users (email, password_hash)
+      VALUES (?, ?)
+    `)
+    .bind(email, passwordHash)
+    .run();
+
+  return c.json({
+    success: result.success
+  });
+});
+
+app.post('/login', async (c) => {
+  const { email, password } = await c.req.json();
+
+  if (!email || !password) {
+    return c.json(
+      { message: 'Email and password are required' },
+      400
+    );
+  }
+
+  const user = await c.env.DB
+    .prepare(`
+      SELECT *
+      FROM users
+      WHERE email = ?
+    `)
+    .bind(email)
+    .first<{
+      id: number;
+      email: string;
+      password_hash: string;
+    }>();
+
+  if (!user) {
+    return c.json(
+      { message: 'Invalid credentials' },
+      401
+    );
+  }
+
+  const isMatch = await comparePassword(
+    password,
+    user.password_hash
+  );
+
+  if (!isMatch) {
+    return c.json(
+      { message: 'Invalid credentials' },
+      401
+    );
+  }
+
+  return c.json({
+    message: 'Login successful'
+  });
+});
 export default app
